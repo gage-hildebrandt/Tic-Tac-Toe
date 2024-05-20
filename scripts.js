23f65a960e52f2bb4366e4811e1a61f1a06916dd
Gage Hildebrandt <vadium3854@gmail.com>
2024-05-19 20:34:14 -0400

    created the 3x3 grid of html/css

---
+++ scripts.js
@@ -0,0 +1,39 @@
+const cells = document.querySelectorAll('.cell');
+const statusText = document.querySelector('#statusText');
+const restartBtn = document.querySelector('#restartBtn');
+const winConditions = [
+
+]
+
+
+
+
+
+
+// let gameBoard = {
+//     board: ['', '', '', '', '', '', '', '', ''], // represents the 3x3 grid board
+
+//     displayBoard: function () {
+//         console.log(this.board.slice(0, 3));
+//         console.log(this.board.slice(3, 6));
+//         console.log(this.board.slice(6, 9));
+//     }
+// };
+
+// function player (symbol) {
+//     this.symbol = symbol;
+
+
+// };
+
+// let player1 = new player('X');
+// let player2 = new player('O');
+
+// let gameController = {
+//     currentPlayer: player1,
+//     gameActive: true,
+
+//     switchPlayer: function() {
+//         this.currentPlayer = this.currentPlayer === player1 ? player2 : player1;
+//     }
+// }
